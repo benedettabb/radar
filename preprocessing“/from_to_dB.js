5d13676cffd41da333062500356f59f4d8d23096
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-07-19 16:46:47 +0200

    Update from_to_dB.js

--- preprocessing“/from_to_dB.js
+++ preprocessing“/from_to_dB.js
@@ -1,8 +1,10 @@
-// Functions to convert from/to dB
+// Funzione che converte l'immagine S1 GRD in sigma naught dB in un'immagine lineare
 exports.toNatural = function (img) {
   return ee.Image(10.0).pow(img.select(0).divide(10.0));
 };
 
+//Funzione che converte l'immagine lineare in immagine in dB. 
+//dB= 10* log (DN)
 exports.toDB = function (img) {
   return ee.Image(img).log10().multiply(10.0);
 };
