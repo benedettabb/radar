701ebcbf027e1df86061e79650ea17d2a5d28ebb
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-07-19 16:48:50 +0200

    Update from_to_dB.js

--- preprocessing“/from_to_dB.js
+++ preprocessing“/from_to_dB.js
@@ -1,3 +1,6 @@
+//Sentinel-1-Imagery-Based High-ResolutionWater Cover Detection on Wetlands, Aided by Google Earth Engine, András Gulácsi and Ferenc Kovács
+//https://github.com/SalsaBoy990/gee_s1_sar_wetlands
+
 // Funzione che converte l'immagine S1 GRD in sigma naught dB in un'immagine lineare
 exports.toNatural = function (img) {
   return ee.Image(10.0).pow(img.select(0).divide(10.0));
