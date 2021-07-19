5b52fea8673a6073dceba70c4ab9ba77197af0b2
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-07-19 17:00:28 +0200

    Update multiTempFilter.js

--- preprocessing“/multiTempFilter.js
+++ preprocessing“/multiTempFilter.js
@@ -1,18 +1,22 @@
-//funzione che applica il refined Lee filter ad un'immagine
-//(con map la si applica ad ogni immagine di una collezione)
+//funzione che applica il refinedLeeFilter alle due bande di un'immagine radar
+//l'outpu è un'immagine con bande "VV filtrata", "VH filtrata", "angle"
+//iterando la funzione con .map() si può applicare il filtro ad ogni immagine di una collezione
 
+//richiede la funzione refinedLeeFilter (cambiare path se necessario!)
 var lee = require ("users/bene96detta/AnalysisReadyData:preprocessing/refinedLeeFilter")
 
+//richiede le funzioni toNatural e toDB (cambiare path se necessario!)
+//il filtro va applicato all'immagine lineare e non a quella in dB (un'altra possibilità sarebbe quella di usare la nuova collezione "S1...GRD_FLOAT")
 var from_to_dB = require ("users/bene96detta/AnalysisReadyData:preprocessing/from_to_dB")
 
 
-//funzione che a partire da una immagine restituisce come output la stessa immagine filtrata con refinedLeeFilter
+//esporta la funzione
 exports.MultiTempFilter = function (img){
-  //filtro la banda VV
+  //filtra la banda VV
   var filteredVV = (from_to_dB.toDB(lee.refinedLee (from_to_dB.toNatural(img.select (['VV'])))))
-  //filtro la banda VH
+  //filtra la banda VH
   var filteredVH = (from_to_dB.toDB(lee.refinedLee (from_to_dB.toNatural(img.select (['VH'])))))
-  //compongo l'immagine
+  //compone l'immagine
   var img_filtered = ee.Image.cat ([
     (filteredVV.select('sum').rename('VV_filtered')), 
     (filteredVH.select('sum').rename('VH_filtered')),
